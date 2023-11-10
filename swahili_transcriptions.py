951bf23692a117cf88dca4ebfb7b8a9d59c11694
Shubham Pilivkar <shubhampilivkar@gmail.com>
2023-11-10 15:21:40 +0530

    Add files via upload

---
+++ swahili_transcriptions.py
@@ -0,0 +1,14 @@
+import requests
+
+headers = {
+    'x-gladia-key': 'f73673b2-2809-4232-9142-9b210bae0231',
+}
+
+files = {
+    'audio_url': (None, 'https://github.com/DriveMyScream/sample/raw/main/Swahili_Language.wav'),
+    'toggle_diarization': (None, 'true'),
+    'language': 'swahili'
+}
+
+response = requests.post('https://api.gladia.io/audio/text/audio-transcription/', headers=headers, files=files)
+print(response.json())
